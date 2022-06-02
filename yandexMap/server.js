765e2180468f770a854e0fadb69c12087145ec69
ElizavetaLisenkova <59146630+ElizavetaLisenkova@users.noreply.github.com>
2022-06-02 09:40:54 +0300

    Delete yandexMap directory

--- yandexMap/server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let app = express();
-
-app.get('/', function (req, res){
-    res.sendFile('/Users/eelisn/Study/веб/yandexMap/index.html')
-});
-
-app.listen(8089);
-
